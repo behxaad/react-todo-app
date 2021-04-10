ad79ebe769146cfa2325bc77dd46f390a6f3fb07
Behzad <behxadbhat@gmail.com>
2021-04-10 14:07:58 +0530

    sceond commmit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TodoList from "./TodoList"
 
 
 class TodoForm extends Component {
@@ -6,7 +7,6 @@ class TodoForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: "",
             items: []
         }
 
@@ -14,29 +14,39 @@ class TodoForm extends Component {
     }
 
     addItem(e) {
-        if(this.input !== "")
-        {
-            newItem = {
-               text: this.input.target.value,
-               key: Date.now()
-            }
+        if (this._inputElement.value !== "") {
+          var newItem = {
+            text: this._inputElement.value,
+            key: Date.now()
+          };
+       
+          this.setState((prevState) => {
+            return { 
+              items: prevState.items.concat(newItem) 
+            };
+          });
+         
+          this._inputElement.value = "";
         }
-
-    }
-
+         
+        console.log(this.state.items);
+           
+        e.preventDefault();
+      }
 
 
     render() {
 
         return (
+            <>
             <form onSubmit={this.addItem}>
                 <input type="text" className="todo-input" placeHolder="Enter Text"
-                    ref = {function(e1) { 
-                        this.input = e1
-                    } //IMPORTANT
-                    } />
+                    ref={(a) => this._inputElement = a}
+                />
                 <button>Add Todo</button>
             </form>
+            <TodoList entries={this.items}/>
+            </>
         )
 
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 
-const TodoList = () => {
-
-    return(
-
-    <div className="todo-div">
-        <ul className="todo-list"></ul>
-    </div>
-
-    )
-
-    
-}
+class TodoList extends Component {
+    createTasks(item) {
+      return <li key={item.key}>{item.text}</li>
+    }
+   
+    render() {
+      var todoEntries = this.props.entries;
+      var listItems = todoEntries.map(this.createTasks);
+   
+      return (
+        <ul className="theList">
+            {listItems}
+        </ul>
+      );
+    }
+  };
 
 export default TodoList

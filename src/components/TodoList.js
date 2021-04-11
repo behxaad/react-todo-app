f2093909aa6c424f61f52c191f7cc0b49936cd00
Behzad <behxadbhat@gmail.com>
2021-04-11 15:57:51 +0530

    git deploy

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
 class TodoList extends Component {
 
@@ -6,16 +7,18 @@ class TodoList extends Component {
     this.props.delete(key);
   }
 
-    render() {
-      var todoEntries = this.props.entries;
-      var listItems = todoEntries.map(item => <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>);
-   
-      return (
-        <ul className="todoList">
-            {listItems}
-        </ul>
-      );
-    }
-  };
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(item => <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>);
+
+    return (
+      <ul className="todoList">
+        <FlipMove duration={350} easing="ease-out">
+          {listItems}
+        </FlipMove>
+      </ul>
+    );
+  }
+};
 
 export default TodoList
